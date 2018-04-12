06e53fbdb6922302651c5f780e02b8c31c9529db
bwielk <bwielk@gmail.com>
2018-04-12 20:48:54 +0200

    Setters work

--- src/todolist/datamodel/ToDoItem.java
+++ src/todolist/datamodel/ToDoItem.java
@@ -1,4 +1,29 @@
 package todolist.datamodel;
 
+import java.time.LocalDate;
+
 public class ToDoItem {
+
+    private String title;
+    private String description;
+
+    ToDoItem(String title, String description){
+        this.title = title;
+        this.description = description;
+    }
+
+    /*
+    ToDoItem(String title, String description){
+        this.title = title;
+        this.description = description;
+
+    }*/
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
 }
--- tests/todolist/datamodel/ToDoItemTest.java
+++ tests/todolist/datamodel/ToDoItemTest.java
@@ -10,12 +10,14 @@ public class ToDoItemTest {
     private ToDoItem item1;
 
     @Before
-    public void setUp() throws Exception {
-        item1 = new ToDoItem();
+    public void before() {
+        item1 = new ToDoItem("Do it tomorrow", "Take the rubbish out and water the plants, please");
     }
 
     @Test
     public void gettersWork(){
-        assertEquals(item1.getClass(), item1.getClass());
+        assertEquals(ToDoItem.class, item1.getClass());
+        assertEquals("Do it tomorrow", item1.getTitle());
+        assertEquals("Take the rubbish out and water the plants, please", item1.getDescription());
     }
 }
